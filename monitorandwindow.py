4c5442fa971bdfbb35e48ec356d3e6b609a4c9ad
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-11-13 23:43:57 -0700

    Update monitorandwindow.py

--- monitorandwindow.py
+++ monitorandwindow.py
@@ -9,4 +9,4 @@ thisHeight = thisSize[1]
 win = visual.Window(monitor=mon, size= (400,400), color = [-1,-1,-1], fullscr=True)
 
 #1) Changing the units for pixel size changes what is used as units for stimuli of window and its percentage of screen.
-#2) Changing color space changes what method is used to define and code color. Colors can be coded by name depending what ColorSpace method used.
+#2) Changing color space changes what method is used to define and code color. Colors can be coded by name web/X11 ColorSpace.
