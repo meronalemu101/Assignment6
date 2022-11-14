2d8bb472a4f4744dd9102bd1c602f7a1489ef5ca
meronalemu101 <113920340+meronalemu101@users.noreply.github.com>
2022-11-13 23:40:21 -0700

    Create monitorandwindow.py

---
+++ monitorandwindow.py
@@ -0,0 +1,12 @@
+mon = monitors.Monitor('myMonitor', width=31.4, distance=60) 
+mon.setSizePix([1280,800])
+mon.save()
+thisSize = mon.getSizePix()
+thisWidth = thisSize[0]
+thisHeight = thisSize[1]
+
+
+win = visual.Window(monitor=mon, size= (400,400), color = [-1,-1,-1], fullscr=True)
+
+#1) Changing the units for pixel size changes what is used as units for stimuli of window and its percentage of screen.
+#2) Changing color space changes what method is used to define and code color. Colors can be coded by name depending what ColorSpace method used.
